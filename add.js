7c6ee92eb0d9205009eff5fff527b2f75802c2d7
ironkamosi <alintonchambers@gmail.com>
2021-09-13 16:25:32 +0100

    boiler plate + first failing test

---
+++ add.js
@@ -0,0 +1,6 @@
+
+function add(a, b) {
+    return 0
+}
+
+module.exports = { add }
---
+++ add.test.js
@@ -0,0 +1,7 @@
+const { add } = require("./add")
+
+describe("addition", () => {
+    it("adds two numbers", ()=> {
+      expect (add(2,3)).toBe(5)  
+    })
+})
